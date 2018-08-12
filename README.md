d5a130c50fa33136eea3c93a7f52c2cdb96cb5d0
Andreas Teufel <a.teufel@gmail.com>
2018-08-12 20:31:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# filter-images
+
+This tool removes images below a specified brightness level from a folder
+
+## Usage
+* Install ImageMagick
+* Install Node.js
+* Install yarn
+* Run `yarn install`
+* Put your images into the `images` folder
+* Set minBrightness in `index.js`
+* Run `node index.js`
---
+++ index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const gm = require('gm').subClass({imageMagick: true});
+const _cliProgress = require('cli-progress');
+const Confirm = require('prompt-confirm');
+
+const prompt = new Confirm('Do you want to proceed?');
+const progressBar = new _cliProgress.Bar({}, _cliProgress.Presets.shades_classic);
+
+const minBrightnessLevel = 0.3;
+
+const getBrightnessLevel = (filename) => {
+	return new Promise(function (resolve, reject) {
+		gm(filename).identify({format:'%[fx:lightness]'}, (err, info) => {
+			if (err) {
+				reject();
+			} else {
+				resolve(parseFloat(info));
+			}
+		});
+	});
+};
+
+(async () => {
+	let images = fs.readdirSync('images');
+	let imagesWithBrightness = [];
+
+	try {
+		console.log('processing images...');
+		progressBar.start(images.length, 0);
+		for (let i = 0; i < images.length; i++) {
+			//detect brightness for every single image
+			const brightness = await getBrightnessLevel(`images/${images[i]}`);
+			imagesWithBrightness[i] = {
+				fileName: images[i],
+				brightness: brightness
+			};
+			progressBar.update(i + 1);
+		}
+		progressBar.stop();
+
+		const imagesToDelete = imagesWithBrightness.filter((elem) => elem.brightness < minBrightnessLevel);
+		console.log(`number of images to delete: ${imagesToDelete.length}`);
+		const answer = await prompt.run();
+		if (answer) {
+			//remove images from folder
+			console.log('deleting images...');
+			progressBar.start(imagesToDelete.length, 0);
+			for (let i = 0; i < imagesToDelete.length; i++) {
+				fs.unlinkSync(`images/${imagesToDelete[i].fileName}`);
+				progressBar.update(i + 1);
+			}
+			progressBar.stop();
+			console.log('done');
+		}
+		process.exit(0);
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+})();
